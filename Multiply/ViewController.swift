87d2367a12cbe737b8cbf20ca6a326b5917add78
Gio Garcia <giogarcia4711@gmail.com>
2019-10-10 12:27:37 -0500

    initial Commit

---
+++ Multiply/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Multiply
+//
+//  Created by GIOVANNI GARCIA on 8/29/19.
+//  Copyright © 2019 GIOVANNI GARCIA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+   
+   
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var uiImageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+    
+    @IBAction func onButtonTapped(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func textField2(_ sender: Any) {
+    }
+    
+   
+    
+
+}
+
